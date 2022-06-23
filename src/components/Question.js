fa8629deb96af1ba45be20f27a1d74ce5dfaea42
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-06-23 10:51:55 +0700

    update question to display

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from "axios";
+
 
 const url = "http://localhost:3000"
 
+
 function Question() {
-    axios.get(`${url}/api/user/`).then((response) => {
-        const allQuiz = response.data[4].question;
-        console.log(allQuiz);
-      }).catch(error => console.error('Error'));
+    const [display, setDisplay] = useState(null);
+    let data; 
+    const getData = () => {
+        axios.get(`${url}/api/user/`).then((res) => {
+            data = res.data;
+            console.log(data);
+            setDisplay(data);
+          });
+      };
+      if (display === null) {
+        getData();
+      }
     
-  return (
-    <>
-    <allQuiz/>
-    </>
-  )
+    return (
+        <div style={{ width: "80%", margin: "20px 10px" }}>
+          <div >
+            {display
+              ? display.map((x) => (
+                  <div key={x}>
+                    
+                    <h4 key={x.question}>
+                      question : {x.question}
+                    </h4>
+                    
+                  </div>
+                ))
+              : ""}
+          
+          </div>
+        </div>
+      );
 }
 
 export default Question;

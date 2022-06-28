a0d47997281c3f88902328493b6f7d226f81126b
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-06-28 11:03:26 +0700

    Update title of Website

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import store from "./components/QuizPage/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
 	<React.StrictMode>
 		<BrowserRouter>
 			<Provider store={store}>
@@ -17,15 +16,6 @@ root.render(
 			</Provider>
 		</BrowserRouter>a
 	</React.StrictMode>
-=======
-  <React.StrictMode>
-    <BrowserRouter>
-      {/* <Provider store={store}> */}
-        <App />
-      {/* </Provider> */}
-    </BrowserRouter>a
-  </React.StrictMode>
->>>>>>> a157f6f46d6e170b71aa86a94928d5d3706e2a05
 );
 
 // If you want to start measuring performance in your app, pass a function

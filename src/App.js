859c0e250a1ee9886545ae2bec513e18bef90ccb
P-Amonrat <amonratpongka@gmail.com>
2022-06-30 16:55:57 +0700

    store answers and user from frontend to localstorage and post to database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Routes, Route} from "react-router-dom";
 import LayoutQuiz from "./components/QuizPage/LayoutQuiz";
 import LayoutProfile from "./components/ProfilePage/LayoutProfile";
 import RegisterForm from "./components/RegisterForm/register";
-// import StrengthSelectedPage from "./components/Strength/StrengthSelectedPage";
+import StrengthSelectedPage from "./components/Strength/StrengthSelectedPage";
+// import axios from "axios";
 // import Choices from "./components/Choices";
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
 				<Route path="/quiz" element={<LayoutQuiz />} />
 				<Route path="/profile" element={<LayoutProfile />} />
 				<Route path="/register" element={<RegisterForm />} />
-				{/* <Route path="/strength" element={<StrengthSelectedPage />} /> */}
+				<Route path="/strength" element={<StrengthSelectedPage />} />
 				{/* <Route path="login" element={<Dashboard />} /> */}
 				{/* </Route> */}
 			</Routes>
--- src/components/QuizPage/QuizItem.js
+++ src/components/QuizPage/QuizItem.js
@@ -5,26 +5,14 @@ import axios from "axios";
 import "./quiz.css";
 import Choices from "./Choices";
 import { Button, Progress, Modal } from "antd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addAnswer } from "./storeReducer";
 import Login from "../Login/Login";
-// import { useNavigate } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const QuizItem = () => {
-	const [isModalVisible, setIsModalVisible] = useState(false);
-
-	const showModal = () => {
-		setIsModalVisible(true);
-	};
-
-	const handleOk = () => {
-		setIsModalVisible(false);
-	};
-
-	const handleCancel = () => {
-		setIsModalVisible(false);
-	};
+	const answers = useSelector(state => state.answer.answers);
+	console.log("answers", answers);
 
 	const dispatch = useDispatch();
 
@@ -37,18 +25,43 @@ const QuizItem = () => {
 	const [choice3, setChoice3] = useState("");
 	const [choice4, setChoice4] = useState("");
 
-
 	const [choiceSelected, setChoiceSelected] = useState([]);
 
 	const [order, setOrder] = useState(["4", "3", "2", "1"]);
 
+	const [isModalVisible, setIsModalVisible] = useState(false);
+
+	// const [id, setID] = useState();
+
+	const showModal = () => {
+		// if (choiceSelected.length !== 4) return;
+		// dispatch(addAnswer([1, id, choiceSelected.join(), 1]));
+		setIsModalVisible(true);
+		console.log("numberShowmodal", number);
+		if (number === 2) {
+			console.log("numberIf", number);
+			const answerStr = JSON.stringify(answers);
+			console.log(answerStr);
+			localStorage.setItem("answers", answerStr);
+		}
+	};
+
+	const handleOk = () => {
+		setIsModalVisible(false);
+	};
+
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
+
 	const handleNext = (id) => {
 		if (choiceSelected.length !== 4) return;
 		dispatch(addAnswer([1, id, choiceSelected.join(), 1]));
-		if (number < 36) {
+		if (number < 37) {
 			setNumber(number + 1);
+			console.log("number", number);
 		}
-		setPercent(number * 2.86);
+		setPercent(number * 2.9);
 
 		setOrder(["4", "3", "2", "1"]);
 		setChoice1("");
@@ -58,19 +71,6 @@ const QuizItem = () => {
 		setChoiceSelected([]);
 	};
 
-	// const handleSubmit = (e) => {
-	// 	const navigate = useNavigate();
-	// 	e.preventDefault();
-	// 	axios.post("http://localhost:3000/api/user/quiz/", {
-	// 		answers: state,
-
-	// 	}).then((response) => {
-	// 		console.log(response);
-	// 		navigate("/profile");
-	// 	}).catch((error) => {
-	// 		console.log(error);
-	// 	});
-	// };
 	const handleClick1 = e => {
 		e.preventDefault();
 		if (!choice1) {
@@ -125,58 +125,73 @@ const QuizItem = () => {
 		<div>
 			<div className="box">
 				<div className="inbox">
-					<div className="bar">
-						<p style={{ margin: "0px", fontSize: "16px" }}>คำถามที่ {number} จาก 35</p>
-						<Progress percent={percent} showInfo={false} />
-					</div>
-					<div>
-						<h5 className="head">
-							{question.map(val =>
-								val.id === number && (
-									`Question ${val.id} : ${val.question}`
+					{number <= 1 ?
+						<div>
+							<div className="bar">
+								<p style={{ margin: "0px", fontSize: "16px" }}>คำถามที่ {number} จาก 35</p>
+								<Progress percent={percent} showInfo={false} />
+							</div>
+							<div>
+								<h5 className="head">
+									{question.map(val =>
+										val.id === number && (
+											`Question ${val.id} : ${val.question}`
+										)
+									)}
+								</h5>
+							</div>
+							<div className="choice-list-grid">
+
+								{question.map(val =>
+									val.id === number && (
+										<>
+											<div onClick={handleClick1}><Choices order={choice1} title={val.choice_1} /></div>
+											<div onClick={handleClick2}><Choices order={choice2} title={val.choice_2} /></div>
+											<div onClick={handleClick3}><Choices order={choice3} title={val.choice_3} /></div>
+											<div onClick={handleClick4}><Choices order={choice4} title={val.choice_4} /></div>
+
+										</>
+									)
 								)
-							)}
-						</h5>
-					</div>
-					<div className="choice-list-grid">
+								}
+							</div>
 
-						{question.map(val =>
-							val.id === number && (
-								<>
-									<div onClick={handleClick1}><Choices order={choice1} title={val.choice_1} /></div>
-									<div onClick={handleClick2}><Choices order={choice2} title={val.choice_2} /></div>
-									<div onClick={handleClick3}><Choices order={choice3} title={val.choice_3} /></div>
-									<div onClick={handleClick4}><Choices order={choice4} title={val.choice_4} /></div>
+							<div className="btn">
+								<button className="list-btn1" onClick={handleReset}>เคลียร์คำตอบ</button>
+								<Button
+									className="list-btn2"
+									onClick={() => handleNext(question[number - 1].id)}
+								>
+									ต่อไป
+								</Button>
+							</div>
 
-								</>
-							)
-						)
-						}
-					</div>
-
-					<div className="btn">
-						<button className="list-btn1" onClick={handleReset}>เคลียร์คำตอบ</button>
-						{number === 35 ? <Button className="list-btn-sent-ans"
-							onClick={showModal}
-						>
-							ส่งคำตอบ
-						</Button> : <Button
-							className="list-btn2"
-							onClick={() => handleNext(question[number - 1].id)}
-						>
-							ต่อไป
-						</Button>}
-					</div>
+						</div>
+						:
+						<div className="finished-ans">
+							<p className="thanks">Thank you for your attention!!</p>
+							{number === 2 ?
+								<div className="sent-ans">
+									<Modal
+										className="modal-login"
+										title="LOGIN" visible={isModalVisible}
+										onOk={handleOk} onCancel={handleCancel}
+										footer={null}
+									>
+										<Login />
+									</Modal>
+									<Button className="list-btn-sent-ans"
+										onClick={showModal}
+									>
+										ส่งคำตอบ
+									</Button>
+								</div>
+								: <></>}
+						</div>
+					}
 				</div>
 			</div>
-			<Modal
-				className="modal-login"
-				title="LOGIN" visible={isModalVisible}
-				onOk={handleOk} onCancel={handleCancel}
-				footer={null}
-			>
-				<Login />
-			</Modal>
+
 		</div>
 
 	);

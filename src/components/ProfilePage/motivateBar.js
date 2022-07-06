1f75adc979dac63a184eb4683835991a9713911c
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-07-06 11:43:43 +0700

    Edit Motivate bar

--- src/components/ProfilePage/motivateBar.js
+++ src/components/ProfilePage/motivateBar.js
@@ -5,23 +5,31 @@ const MotivateBar = () => {
 	return (
 		<div style={{ display : "flex", width : "100%" }}>
 			<div className="left-side-bar">
-				<h5>การช่วยเหลือผู้คน</h5>
+				<h5>Helping</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
-				<h5>อิสระเสรี</h5>
+				<h5>Supporting</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
-				<h5>การช่วยเหลือผู้คน</h5>
+				<h5>knowledge</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
-				<h5>อิสระเสรี</h5>
+				<h5>Order</h5>
+				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
+				<h5>Service</h5>
+				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
+				<h5>Belonging</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
 			</div>
 			<div className="right-side-bar">
-				<h5>การช่วยเหลือผู้คน</h5>
+				<h5>Self Reliance</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
-				<h5>อิสระเสรี</h5>
+				<h5>Influence</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
-				<h5>การช่วยเหลือผู้คน</h5>
+				<h5>Practicality</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
-				<h5>อิสระเสรี</h5>
+				<h5>Freedom</h5>
+				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
+				<h5>Financial Security</h5>
+				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
+				<h5>Uniqueness</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
 			</div>
 		</div>

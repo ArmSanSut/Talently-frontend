88f632e9ec98c2e8332f8a2e199e44d22126dbf2
Inarm <mramrasansut@outlook.co.th>
2022-07-06 16:40:03 +0700

    feature:get score to display in progress bar

--- src/components/ProfilePage/environmentBar.js
+++ src/components/ProfilePage/environmentBar.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React,  { useEffect, useState } from "react";
 import { Progress } from "antd";
 import "./environmentBar.css";
+import axios from "axios";
+
 const EnvironmentBar = () => {
+	const id = localStorage.getItem("ID");
+	// const [, setLoad] = useState(true);
+	const [percent, setPercent] = useState([]);
+
+	const UserScore = async ()=> {
+		// setLoad(false);
+		const getScore = await axios.get(`http://localhost:3000/api/user/score/${id}`);
+		const totalScore = getScore.data[0].score.score;
+		const scoreM = totalScore.scoreM;
+		// const [tempValue, ] = useState([]);
+		const temp = [];
+		
+		for (let index = 0; index < 6; index++) {
+			let mFirst = scoreM[`M${2*index + 1}`];
+			let mSecond = scoreM[`M${2*index + 2}`];
+			let score = mFirst - mSecond;
+			let percentDisplay = ((mFirst + mSecond)/40)*100;
+			// console.log(score);
+			if (score > 0 ){
+
+				temp.push(["L", percentDisplay]);
+	
+			}else if (score < 0) {
+
+				temp.push(["R", percentDisplay]);
+					
+			}else {
+
+				temp.push(["E", percentDisplay]);
+					
+			}
+			console.log(index);
+		}
+		setPercent(temp);
+		// return temp;
+	};
+		
+	console.log(percent);
+	useEffect(() => {
+		UserScore();
+
+		console.log(percent.length);	
+	}, []);
+
 	return (
 		<div style={{ display : "flex", width : "100%" }}>
 			<div className="left-side-bar">
--- src/components/ProfilePage/motivateBar.js
+++ src/components/ProfilePage/motivateBar.js
@@ -1,12 +1,18 @@
-import React from "react";
+/* eslint-disable no-empty */
+import React  from "react";
 import { Progress } from "antd";
 import "./motivateBar.css";
+// import axios from "axios";
 const MotivateBar = () => {
+
+	
+
 	return (
 		<div style={{ display : "flex", width : "100%" }}>
+			
 			<div className="left-side-bar">
 				<h5>การช่วยเหลือผู้คน</h5>
-				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
+				<Progress percent={40} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
 				<h5>อิสระเสรี</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-left"/>
 				<h5>การช่วยเหลือผู้คน</h5>
@@ -16,7 +22,7 @@ const MotivateBar = () => {
 			</div>
 			<div className="right-side-bar">
 				<h5>การช่วยเหลือผู้คน</h5>
-				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
+				<Progress percent={40} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
 				<h5>อิสระเสรี</h5>
 				<Progress percent={50} showInfo={false} trailColor="#F3F8FF" className="progress-display-blue-right"/>
 				<h5>การช่วยเหลือผู้คน</h5>
